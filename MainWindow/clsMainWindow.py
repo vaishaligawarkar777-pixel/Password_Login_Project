b09974de88140d17be9e1b2e6ed033a9e2d44143
vaishaligawarkar777-pixel <vaishaligawarkar777@gmail.com>
2025-10-23 17:04:20 +0530

    Delete some data.

--- MainWindow/clsMainWindow.py
+++ MainWindow/clsMainWindow.py
@@ -3,6 +3,8 @@ from MainWindow.MainWindow import Ui_MainWindow
 from Add_User.clsAdd_User import clsAdd_User
 from Delete_User.clsDelete_User import clsDelete_User
 from Change_Password.clsChange_Password import clsChange_Password
+from Student.clsStudent import clsStudentForm
+
 
 class clsMainWindow(QMainWindow):
     def __init__(self):
@@ -12,6 +14,9 @@ class clsMainWindow(QMainWindow):
 
         self.ui.actionAdd_User.triggered.connect(self.Add_UserClick)
         self.ui.actionChange_Password.triggered.connect(self.Change_PasswordClick)
+        self.ui.actionDelete_User.triggered.connect(self.Delete_UserClick)
+        self.ui.actionStudent_File.triggered.connect(self.Student_UserClick)
+        self.ui.actionFees_Form.triggered.connect(self.Fees_UserClick)
         self.ui.actionDelete_User.triggered.connect(self.Delete_UserClick)
 
     def Add_UserClick(self):
@@ -29,3 +34,20 @@ class clsMainWindow(QMainWindow):
         self.c1 = clsChange_Password()
         self.ui.mdiArea.addSubWindow(self.c1)
         self.c1.show()
+
+    def Student_UserClick(self):
+        self.s1 = clsStudentForm()
+        self.ui.mdiArea.addSubWindow(self.s1)
+        self.s1.show()
+
+    def Fees_UserClick(self):
+        self.f1 =clsFees()
+        self.ui.mdiArea.addSubWindow(self.f1)
+        self.f1.show()
+
+
+
+
+
+
+
